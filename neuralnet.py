Fixed backprop algorithm, changed hyperparameters for initial model

neuralnet.py
@@ -119,7 +119,6 @@ class Layer:
         TODO in case you want to add variables here
         Define the architecture and create placeholders.
         """
-
         self.w = None
         if weight_type == 'random':
             self.w = 0.01 * np.random.random((in_units + 1, out_units))
@@ -161,16 +160,18 @@ class Layer:
         gradReqd=True means update self.w with self.dw. gradReqd=False can be helpful for Q-3b
         """
         if self.activation.activation_type != 'output':
-            deltaCur = deltaCur[1:]
-        delta = np.sum(self.activation.backward(self.a) * deltaCur, axis=0) / self.x.shape[0]
+            deltaCur = deltaCur[:, 1:]
+        delta = self.activation.backward(self.a) * deltaCur
+        # delta = np.mean(self.activation.backward(self.a) * deltaCur, axis=0)
         L2_penalty = 2 * regularization * self.w
-        delta_next = self.w @ delta
-        grad = - (np.outer(delta, np.ones(self.x.shape[0])) @ self.x) / self.x.shape[0]
+        delta_next = delta @ self.w.T
+        grad = -self.x.T @ delta + L2_penalty
+        # grad = - np.outer(np.mean(self.x, axis=0), delta) + L2_penalty
+
+        self.dw = - (learning_rate / self.x.shape[0]) * grad + momentum_gamma * self.dw
 
         if gradReqd:
-            self.w = self.w - learning_rate * (np.transpose(grad) + L2_penalty) + momentum_gamma * self.dw
-
-        self.dw = - learning_rate * (np.transpose(grad) + L2_penalty) + momentum_gamma * self.dw
+            self.w = self.w + self.dw
 
         return delta_next
 
@@ -232,7 +233,7 @@ class Neuralnetwork:
         '''
         TODO: compute the categorical cross-entropy loss and return it.
         '''
-        return - np.dot(targets.flatten(), np.log(logits, where=logits > 0).flatten()) / logits.shape[0]
+        return - np.dot(targets.flatten(), np.log(logits).flatten()) / logits.shape[0]
 
     def backward(self, gradReqd=True):
         '''

train.py
@@ -28,24 +28,45 @@ def train(model, x_train, y_train, x_valid, y_valid, config):
     train_acc = []
     val_loss = []
     val_acc = []
-    for epoch in tqdm(range(100)):
+    best_loss_epoch = 0
+    best_loss = np.inf
+    epoch_increase_counter = 0
+
+    for epoch in tqdm(range(config['epochs'])):
         x_train, y_train = util.shuffle((x_train, y_train))
         epoch_loss = []
         epoch_acc = []
-        for mini_batch in generate_minibatches((x_train, y_train)):
+        for mini_batch in generate_minibatches((x_train, y_train), batch_size=config['batch_size']):
             acc, loss = model.forward(mini_batch[0], mini_batch[1])
             epoch_loss.append(loss)
             epoch_acc.append(acc)
             model.backward()
-        train_loss.append(sum(epoch_loss) / len(epoch_loss))
-        train_acc.append(sum(epoch_acc) / len(epoch_acc))
+        train_loss.append(np.mean(epoch_loss))
+        train_acc.append(np.mean(epoch_acc))
+
         val_acc_epoch, val_loss_epoch = model.forward(x_valid, y_valid)
         val_loss.append(val_loss_epoch)
         val_acc.append(val_acc_epoch)
 
-    util.plots(train_loss, train_acc, val_loss, val_acc, -1)
+        if config['early_stop']:
+            if val_loss_epoch < best_loss:
+                best_loss = val_loss_epoch
+                best_loss_epoch = epoch
+                best_model = copy.deepcopy(model)
+                epoch_increase_counter = 0
+            elif val_loss_epoch > val_loss[-2]:
+                epoch_increase_counter += 1
+            else:
+                epoch_increase_counter = 0
+            if epoch_increase_counter >= config['early_stop_epoch']:
+                break
 
-    return model
+    if not config['early_stop']:
+        best_model = model
+
+    util.plots(train_loss, train_acc, val_loss, val_acc, best_loss_epoch)
+
+    return best_model
 
 #This is the test method
 def modelTest(model, X_test, y_test):